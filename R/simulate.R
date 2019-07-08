a3c133d41845a1a4dee7be1137101c173b28ce11
jtrecenti <julio.trecenti@gmail.com>
2019-07-08 12:37:38 -0300

    dont depend on abind, plyr and purrr

--- R/simulate.R
+++ R/simulate.R
@@ -1,19 +1,23 @@
-
 #' Simulate LTM model
 #'
-#' Simulate LTM model
+#' Simulate LTM model using many
 #'
 #' @param ns number of times
 #' @param nk number of covariates
 #' @param ni number of series
 #' @param vmu vector mu
-#' @param mPhi phi matrix
+#' @param mPhi phi diagonal matrix with the parameters
 #' @param mSigs sigma eta vector
 #' @param dsig general sigma
 #' @param vd threshold parameter
 #' @param alpha intercept
 #'
-#' @return list containing the generated y, x, beta and thresholded beta
+#' @return List containing the generated y, x, beta and thresholded beta.
+#'
+#' @references
+#' Nakajima, Jouchi, and Mike West. "Bayesian analysis of latent threshold
+#' dynamic models." Journal of Business & Economic
+#' Statistics 31.2 (2013): 151-164.
 #'
 #' @export
 ltm_sim <- function(ns, nk, ni, vmu, mPhi, mSigs, dsig, vd, alpha) {
@@ -30,10 +34,8 @@ ltm_sim <- function(ns, nk, ni, vmu, mPhi, mSigs, dsig, vd, alpha) {
   # beta zerado
   mb_zerado <- mb * t(apply(abs(mb), 1, function(x) x > vd))
   # resposta
-
-  xbs <- plyr::aaply(mx, 1, function(x) x * mb_zerado, .drop = FALSE)
-
-  vy <- alpha + plyr::aaply(xbs, 1:2, sum) + dsig * stats::rnorm(ns*ni)
+  xbs <- t(apply(mx, 1, function(z) Rfast::rowsums(z * mb_zerado)))
+  vy <- alpha + xbs + dsig * stats::rnorm(ns*ni)
 
   # resultados
   list(vy = vy, mx = mx, mb = mb, mb_zerado = mb_zerado)
@@ -45,10 +47,35 @@ ltm_pred <- function(post, newdata) {
   nm <- colnames(post)
   ni <- sum(grepl("alpha", nm))
   nk <- sum(grepl("phi", nm))
-  purrr::map(seq_len(n_iter), ltm_pred_iter, post = post,
-             nm = nm, ni = ni, nk = nk, newdata = newdata) %>%
-    abind::abind(along = 3) %>%
-    aperm(c(3, 1, 2))
+  nt <- dim(newdata)[2]
+  soma <- array(0, dim = c(ni, nt))
+  for(iter in seq_len(n_iter)) {
+    # recuperando parametros
+    post_i <- post[iter, ]
+    mb <- matrix(post_i[grepl("beta", nm)], ncol = nk)
+    mPhi <- post_i[grepl("phi", nm)] * diag(nk)
+    mSigs <- post_i[grepl("sig_eta", nm)]
+    vd <- post_i[grepl("d\\[", nm)]
+    dsig <- post_i[grepl("sig\\[", nm)]
+    alpha <- post_i[grepl("alpha\\[", nm)]
+    vmu <- post_i[grepl("mu\\[", nm)]
+
+    # betas variando no tempo
+    mb_new <- matrix(0, nrow = nt, ncol = nk)
+    # valor do primeiro beta
+    mb_new[1,] <- t(vmu + (chol(solve(diag(nk) - mPhi^2)) * mSigs) %*% stats::rnorm(nk))
+    # equacao do beta
+    for (i in seq_len(nt-1)) {
+      mb_new[1,] <- t(vmu + (chol(solve(diag(nk) - mPhi^2)) * mSigs) %*% stats::rnorm(nk))
+    }
+    # beta zerado
+    mb_zerado <- mb_new * t(apply(abs(mb_new), 1, function(x) x > vd))
+    # resposta
+    xbs <- t(apply(newdata, 1, function(z) Rfast::rowsums(z * mb_zerado)))
+    vy <- alpha + xbs + dsig * stats::rnorm(nt*ni)
+    soma <- soma + vy
+  }
+  soma / n_iter
 }
 
 ltm_pred_iter <- function(iter, post, nm, ni, nk, newdata) {
@@ -75,8 +102,8 @@ ltm_pred_iter <- function(iter, post, nm, ni, nk, newdata) {
   # beta zerado
   mb_zerado <- mb_new * t(apply(abs(mb_new), 1, function(x) x > vd))
   # resposta
-  xbs <- plyr::aaply(newdata, 1, function(x) x * mb_zerado, .drop = FALSE)
-  vy <- alpha + plyr::aaply(xbs, 1:2, sum) + dsig * stats::rnorm(ns_future*ni)
+  xbs <- t(apply(newdata, 1, function(z) Rfast::rowsums(z * mb_zerado)))
+  vy <- alpha + xbs + dsig * stats::rnorm(ns_future*ni)
   vy
 }
 
